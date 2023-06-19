858e80fb97fb40c35b094d4b6347761c12cae829
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-06-19 08:31:14 +0200

    plugs(ufo): add

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -67,17 +67,13 @@ local spec = {
         statuscol.setup(opts)
       end,
     },
-    -- Folding preview, by default h and l keys are used.
-    -- On first press of h key, when cursor is on a closed fold, the preview will be shown.
-    -- On second press the preview will be closed and fold will be opened.
-    -- When preview is opened, the l key will close it and open fold. In all other cases these keys will work as usual.
-    {
-      "anuvyklack/fold-preview.nvim",
-      dependencies = "anuvyklack/keymap-amend.nvim",
-      config = true,
-    },
   },
   event = "BufRead",
+  cond = function()
+    local excluded_filetypes = { "help", "neo-tree", "edgy" }
+    -- if the filetype is in the excluded_filetypes, don't load this plugin
+    return vim.tbl_contains(excluded_filetypes, vim.bo.filetype)
+  end,
   keys = {
     {
       "zR",
