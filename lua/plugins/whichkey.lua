9fc76464e50837c66308d4fbf199fc6f08c4bf6a
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-01 17:56:12 +0200

    plugs(wk): update opts

---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,45 @@
+local spec = {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    plugins = {
+      marks = true, -- shows a list of your marks on ' and `
+      registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+      spelling = true,
+    },
+    key_labels = {
+      ["<space>"] = "SPC",
+      ["<cr>"] = "RET",
+      ["<tab>"] = "TAB",
+    },
+    icons = {
+      breadcrumb = "/",
+      separator = "·",
+      group = "",
+    },
+    popup_mappings = {
+      scroll_down = "<c-d>",
+      scroll_up = "<c-u>",
+    },
+    window = {
+      border = "single",
+      position = "bottom",
+      margin = { 1, 0, 1, 0 },
+      padding = { 1, 1, 1, 1 },
+      winblend = 0,
+    },
+    layout = {
+      height = { min = 5, max = 10 },
+      width = { min = 20, max = 50 },
+      spacing = 5,
+      align = "center",
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    wk.register(opts.defaults)
+  end,
+}
+
+return spec
