d67b9ea225e1d7f557fdc388437476fd65fe5297
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-01 21:06:08 +0200

    plugs(edgy/neotree): update

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -1,105 +1,102 @@
 return {
-  -- edgy
-  {
-    "folke/edgy.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.opt.laststatus = 3
-      vim.opt.splitkeep = "screen"
-    end,
-    opts = {
-      ---@type table<Edgy.Pos, {size:integer, wo?:vim.wo}>
-      options = {
-        left = { size = 40 },
-        bottom = { size = 10 },
-        right = { size = 40 },
-        top = { size = 10 },
-      },
-      bottom = {
-        {
-          ft = "toggleterm",
-          size = { height = 0.4 },
-          filter = function(buf, win)
-            return vim.api.nvim_win_get_config(win).relative == ""
-          end,
-        },
-        {
-          ft = "noice",
-          size = { height = 0.4 },
-          filter = function(buf, win)
-            return vim.api.nvim_win_get_config(win).relative == ""
-          end,
-        },
-        {
-          ft = "lazyterm",
-          title = "LazyTerm",
-          size = { height = 0.4 },
-          filter = function(buf)
-            return not vim.b[buf].lazyterm_cmd
-          end,
-        },
-        "Trouble",
-        { ft = "qf", title = "QuickFix" },
-        {
-          ft = "help",
-          size = { height = 20 },
-          -- don't open help files in edgy that we're editing
-          filter = function(buf)
-            return vim.bo[buf].buftype == "help"
-          end,
-        },
-        { ft = "spectre_panel", size = { height = 0.4 } },
-        {
-          title = "Neotest Output",
-          ft = "neotest-output-panel",
-          size = { height = 15 },
-        },
-      },
-      left = {
-        {
-          title = "Neo-Tree",
-          ft = "neo-tree",
-          filter = function(buf)
-            return vim.b[buf].neo_tree_source == "filesystem"
-          end,
-          pinned = true,
-          open = function()
-            vim.api.nvim_input("<esc><space>e")
-          end,
-          size = { height = 0.5 },
-        },
-      },
-      right = {
-        {
-          title = "Neo-Tree Git",
-          ft = "neo-tree",
-          filter = function(buf)
-            return vim.b[buf].neo_tree_source == "git_status"
-          end,
-          pinned = true,
-          open = "Neotree position=right git_status",
-        },
-        {
-          title = "Neo-Tree Buffers",
-          ft = "neo-tree",
-          filter = function(buf)
-            return vim.b[buf].neo_tree_source == "buffers"
-          end,
-          pinned = true,
-          open = "Neotree position=top buffers",
-        },
+  "folke/edgy.nvim",
+  event = "VeryLazy",
+  enabled = true,
+  init = function()
+    vim.opt.laststatus = 3
+    vim.opt.splitkeep = "screen"
+  end,
+  opts = {
+    ---@type table<Edgy.Pos, {size:integer, wo?:vim.wo}>
+    animate = {
+      enabled = false,
+    },
+
+    options = {
+      left = { size = 40 },
+      bottom = { size = 10 },
+      right = { size = 40 },
+      top = { size = 10 },
+    },
+
+    left = {
+      {
+        title = "Neo-Tree",
+        ft = "neo-tree",
+        filter = function(buf, win)
+          local isFloat = vim.api.nvim_win_get_config(win).relative == ""
+          return isFloat and vim.b[buf].neo_tree_source == "filesystem"
+        end,
+        pinned = true,
+        open = function()
+          vim.api.nvim_input("<esc><space>e")
+        end,
+        size = { height = 0.5 },
       },
     },
-  },
 
-  -- prevent neo-tree from opening files in edgy windows
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.open_files_do_not_replace_types = opts.open_files_do_not_replace_types
-        or { "terminal", "Trouble", "qf", "Outline" }
-      table.insert(opts.open_files_do_not_replace_types, "edgy")
-    end,
+    right = {
+      {
+        title = "Neo-Tree Git",
+        ft = "neo-tree",
+        filter = function(buf, win)
+          local isFloat = vim.api.nvim_win_get_config(win).relative == ""
+          return isFloat and vim.b[buf].neo_tree_source == "git_status"
+        end,
+        pinned = true,
+        open = "Neotree position=right git_status",
+      },
+      {
+        title = "Neo-Tree Buffers",
+        ft = "neo-tree",
+        filter = function(buf, win)
+          local isFloat = vim.api.nvim_win_get_config(win).relative == ""
+          return isFloat and vim.b[buf].neo_tree_source == "buffers"
+        end,
+        pinned = true,
+        open = "Neotree position=top buffers",
+      },
+    },
+
+    bottom = {
+      {
+        ft = "toggleterm",
+        size = { height = 0.4 },
+        filter = function(buf, win)
+          return vim.api.nvim_win_get_config(win).relative == ""
+        end,
+      },
+      {
+        ft = "noice",
+        size = { height = 0.4 },
+        filter = function(buf, win)
+          return vim.api.nvim_win_get_config(win).relative == ""
+        end,
+      },
+      {
+        ft = "lazyterm",
+        title = "LazyTerm",
+        size = { height = 0.4 },
+        filter = function(buf)
+          return not vim.b[buf].lazyterm_cmd
+        end,
+      },
+      "Trouble",
+      { ft = "qf", title = "QuickFix" },
+      {
+        ft = "help",
+        size = { height = 20 },
+        -- don't open help files in edgy that we're editing
+        filter = function(buf)
+          return vim.bo[buf].buftype == "help"
+        end,
+      },
+      { ft = "spectre_panel", size = { height = 0.4 } },
+      {
+        title = "Neotest Output",
+        ft = "neotest-output-panel",
+        size = { height = 15 },
+      },
+    },
   },
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,21 +1,53 @@
 local spec = {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    -- Fix for NerdFonts v3
-    -- https://github.com/nvim-neo-tree/neo-tree.nvim#configuration-for-nerd-fonts-v3-users
-    default_component_configs = {
-      icon = {
-        folder_empty = "󰜌",
-        folder_empty_open = "󰜌",
-      },
-      git_status = {
-        symbols = {
-          renamed = "󰁕",
-          unstaged = "󰄱",
-        },
-      },
+  keys = {
+    {
+      "<leader>ee",
+      function()
+        vim.cmd("Neotree left toggle")
+      end,
+      desc = "Float Git Tree",
+    },
+    {
+      "<leader>eg",
+      function()
+        vim.cmd("Neotree right git_status toggle")
+      end,
+      desc = "Float Git Tree",
+    },
+    {
+      "<C-g>",
+      function()
+        vim.cmd("Neotree float git_status toggle reveal")
+      end,
+      desc = "Float Git Tree",
+    },
+    {
+      "<C-e>",
+      function()
+        vim.cmd("Neotree float buffers toggle reveal")
+      end,
+      desc = "Float Buffer Tree",
     },
   },
+  opts = function()
+    return {
+      -- Fix for NerdFonts v3
+      -- https://github.com/nvim-neo-tree/neo-tree.nvim#configuration-for-nerd-fonts-v3-users
+      default_component_configs = {
+        icon = {
+          folder_empty = "󰜌",
+          folder_empty_open = "󰜌",
+        },
+        git_status = {
+          symbols = {
+            renamed = "󰁕",
+            unstaged = "󰄱",
+          },
+        },
+      },
+    }
+  end,
 }
 
 return spec
