1671e5d32dc98d38084c2b5c2660f085630cec34
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-01 18:13:53 +0200

    lazy: add mini.starter and mini.animate

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,8 +17,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.editor.flash" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.ui.mini-starter" },
     -- import/override with your plugins
     { import = "plugins" },
   },
