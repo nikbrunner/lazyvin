d3b2ceaa2b5ec834d7ff3c72d71a61c47fe3a9d8
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-01 19:29:06 +0200

    lspconfig: add trouble lsp_references

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,11 @@
 ---@type LazySpec
 local spec = {
   "neovim/nvim-lspconfig",
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- change a keymap
+    keys[#keys + 1] = { "gR", "<cmd>Trouble lsp_references<cr>", desc = "LSP References" }
+  end,
   ---@class PluginLspOpts
   opts = {
     capabilities = {
