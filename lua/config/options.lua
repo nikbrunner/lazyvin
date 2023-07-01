27a7d27ab8405d0a5c6f59f2650286416689e72c
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-01 20:51:59 +0200

    opts: global whichkey ignore label

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Global WhichKeyIgnore label
+WhichKeyIgnoreLabel = "which_key_ignore"
+
 vim.opt.foldlevel = 10
 vim.opt.foldmethod = "indent" -- moved to ufo.lua
