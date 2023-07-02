b3a4f81e29b3444a61513f7347d3c33a9170dbbb
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-02 10:21:01 +0200

    plugs(terra): update

---
+++ lua/plugins/terra_core.lua
@@ -0,0 +1,43 @@
+---@type LazySpec
+local terra_spec = {
+  "terra-theme/terra-core.nvim",
+
+  -- TODO: Fix Module Name
+  -- The lua module folder is called `terra`, but the module name is `terra-core`
+  -- This would also allow us to use `require("terra")` instead of `require("terra-core")`
+  -- This would also allow setting up the plugin without the `config` function
+  -- Check how other people are doing it
+  dev = false,
+  lazy = false,
+  priority = 1000,
+
+  ---@type TerraConfig
+  opts = {
+    theme = "winter",
+    variant = "night",
+    transparent = false,
+    diagnostics = {
+      background = true,
+    },
+  },
+
+  config = function(_, opts)
+    local terra = require("terra")
+    terra.setup(opts)
+  end,
+}
+
+---@type LazySpec
+local whichkey_spec = {
+  "folke/which-key.nvim",
+  opts = {
+    defaults = {
+      ["<leader>T"] = { name = "+terra" },
+    },
+  },
+}
+
+return {
+  terra_spec,
+  whichkey_spec,
+}
