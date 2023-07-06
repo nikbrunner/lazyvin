3dccc8d01ab675590e14944db59a5c1c0ad16a5d
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-06 12:58:49 +0200

    plugs(glance): add

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,8 +3,13 @@ local spec = {
   "neovim/nvim-lspconfig",
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- change a keymap
-    keys[#keys + 1] = { "gR", "<cmd>Trouble lsp_references<cr>", desc = "LSP References" }
+
+    -- If `glance.nvim` is available then use it for LSP References
+    if require("lazyvim.util").has("glance.nvim") then
+      keys[#keys + 1] = { "gr", "<cmd>Glance references<CR>", desc = "LSP References (Glance)" }
+    end
+
+    keys[#keys + 1] = { "gR", "<cmd>Trouble lsp_references<CR>", desc = "LSP References (Trouble)" }
   end,
   ---@class PluginLspOpts
   opts = {
