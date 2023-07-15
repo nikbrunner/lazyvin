847b7e543fa1e29af21738c51eb58832a7d680b1
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-15 20:48:48 +0200

    maps: use set/del to clean up LazyVin Group

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,17 +1,22 @@
-local map = vim.keymap.set
+local Util = require("lazyvim.util")
+local set = vim.keymap.set
+local del = vim.keymap.del
 
-map({ "n", "v" }, "H", "^", { desc = "Move to Start of Line" })
-map({ "n", "v" }, "L", "$", { desc = "Move to End of Line" })
+set({ "n", "v" }, "H", "^", { desc = "Move to Start of Line" })
+set({ "n", "v" }, "L", "$", { desc = "Move to End of Line" })
 
-map("n", "<leader><CR>", vim.cmd.wa, { desc = "Save" })
+set("n", "<leader><CR>", vim.cmd.wa, { desc = "Save" })
 
-map("n", "vv", "^v$", { desc = "Select Line" })
-map("n", "vA", "ggVG", { desc = "Select All" })
-map("n", "yA", "ggVGy", { desc = "Copy All" })
+set("n", "vv", "^v$", { desc = "Select Line" })
+set("n", "vA", "ggVG", { desc = "Select All" })
+set("n", "yA", "ggVGy", { desc = "Copy All" })
 
--- LazyVin Group
-map("n", "<leader>l", "<nop>", { desc = WhichKeyIgnoreLabel })
-map("n", "<leader>lm", "<cmd>Mason<CR>", { desc = "Package Manager - [Mason]" })
-map("n", "<leader>ln", "<cmd>NullLsInfo<CR>", { desc = "Tool Manager - [NullLS]" })
-map("n", "<leader>ll", "<cmd>Lazy<CR>", { desc = "Plugin Manager - [Lazy]" })
-map("n", "<leader>li", "<cmd>LspInfo<CR>", { desc = "Lsp Info" })
+-- use "<leader>l" for LazyVin Group
+del("n", "<leader>l")
+del("n", "<leader>L")
+
+set("n", "<leader>lm", "<cmd>Mason<CR>", { desc = "Package Manager - [Mason]" })
+set("n", "<leader>ln", "<cmd>NullLsInfo<CR>", { desc = "Tool Manager - [NullLS]" })
+set("n", "<leader>ll", "<cmd>Lazy<CR>", { desc = "Plugin Manager - [Lazy]" })
+set("n", "<leader>li", "<cmd>LspInfo<CR>", { desc = "Lsp Info" })
+set("n", "<leader>lc", Util.changelog, { desc = "LazyVim Changelog" })
