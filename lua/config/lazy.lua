ddb8c36f4359c83bf8512b9b6c002d792d7776ff
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-15 19:25:36 +0200

    lazy: remove flash as extra (now default)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,6 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
