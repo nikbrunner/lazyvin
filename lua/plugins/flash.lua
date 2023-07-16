ffee4d4cf7880373e9bd62d805f148b224d3f64a
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-16 09:38:01 +0200

    keys(flash): simplify

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -7,27 +7,8 @@ M.spec = {
   ---@type Flash.Config
   opts = {},
   keys = {
-    -- Disable default mappings
-    { "s", mode = { "n", "x", "o" }, false, desc = "Flash" },
-    { "S", mode = { "n", "o", "x" }, false, desc = "Flash Treesitter" },
     {
-      "ss",
-      mode = { "n", "x", "o" },
-      function()
-        require("flash").jump()
-      end,
-      desc = "Flash",
-    },
-    {
-      "st",
-      mode = { "n", "o", "x" },
-      function()
-        require("flash").treesitter()
-      end,
-      desc = "Flash Treesitter",
-    },
-    {
-      "sw",
+      "W",
       mode = { "n", "x" },
       function()
         require("flash").jump({
@@ -35,42 +16,6 @@ M.spec = {
         })
       end,
       desc = "Flash from Word",
-    },
-    {
-      "sl",
-      mode = { "n", "x" },
-      function()
-        require("flash").jump({
-          search = { mode = "search", max_length = 0 },
-          label = { after = { 0, 0 } },
-          pattern = "^",
-        })
-      end,
-      desc = "Jump to line",
-    },
-    {
-      "r",
-      mode = "o",
-      function()
-        require("flash").remote()
-      end,
-      desc = "Remote Flash",
-    },
-    {
-      "R",
-      mode = { "o", "x" },
-      function()
-        require("flash").treesitter_search()
-      end,
-      desc = "Flash Treesitter Search",
-    },
-    {
-      "<c-s>",
-      mode = { "c" },
-      function()
-        require("flash").toggle()
-      end,
-      desc = "Toggle Flash Search",
     },
   },
 }
