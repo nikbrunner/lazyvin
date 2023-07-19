e828eb14caa391b6ce07dd868c9678706cfc48da
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-19 16:12:43 +0200

    arch: move all theme specs into themes.lua (#LVIN-5)

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,7 +1,33 @@
 local M = {}
 
 M.specs = {
-  ---@type LazySpec
+  {
+    "terra-theme/terra-core.nvim",
+
+    dev = true,
+    lazy = false,
+    priority = 1000,
+
+    keys = {
+      {
+        "<leader>lTt",
+        function()
+          require("terra-core.actions.config").select_theme()
+        end,
+        desc = "Select theme",
+      },
+    },
+
+    ---@type TerraConfig
+    opts = {
+      theme = "summer",
+      variant = "night",
+      diagnostics = {
+        background = true,
+      },
+    },
+  },
+
   {
     "rebelot/kanagawa.nvim",
     opts = {
