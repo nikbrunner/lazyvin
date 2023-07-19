e6063f32ab8c0f0c0a700fdd45276538984734eb
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-07-19 16:02:27 +0200

    plugs(kanagawa): add
    squash

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,7 +1,6 @@
 return {
   "LazyVim/LazyVim",
   opts = {
-    ---@alias Colorscheme "tokyonight-moon" | "github_dark_dimmed" | "terra"
-    colorscheme = "tokyonight-moon",
+    colorscheme = "kanagawa-wave",
   },
 }
