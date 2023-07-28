966ac6659bbbbdacee04ff1d76c2d38684b2616b
nbr <nikolaus.brunner@protonmail.ch>
2023-07-28 18:36:13 +0200

    lib(edit): refactor

--- lua/lib/edit.lua
+++ lua/lib/edit.lua
@@ -3,29 +3,35 @@ local utils = require("lib.utils")
 local M = {}
 
 local AUTO_LOG_PREFIX = "Test"
+print(vim.inspect("Test: edit.lua [[AUTO_LOG_PREFIX]]", AUTO_LOG_PREFIX))
 
-local get_auto_print_cmd = function(filetype)
-  if
-    filetype == "typescript"
-    or filetype == "typescriptreact"
-    or filetype == "javascript"
-    or filetype == "javascriptreact"
-  then
-    -- Return the print command for JavaScript-like languages
-    return 'console%.log%("' .. AUTO_LOG_PREFIX
-  elseif filetype == "go" then
-    -- Return the print command for Go
-    return 'fmt%.Println%("' .. AUTO_LOG_PREFIX
-  elseif filetype == "lua" then
-    -- Return the print command for Lua
-    return 'print%("' .. AUTO_LOG_PREFIX
-  end
-end
+local filetype_log_map = {
+  typescript = function(message)
+    return "console.log" .. "(" .. message .. ")"
+  end,
+  typescriptreact = function(message)
+    return "console.log" .. "(" .. message .. ")"
+  end,
+  javascript = function(message)
+    return "console.log" .. "(" .. message .. ")"
+  end,
+  javascriptreact = function(message)
+    return "console.log" .. "(" .. message .. ")"
+  end,
+  astro = function(message)
+    return "console.log" .. "(" .. message .. ")"
+  end,
+  go = function(message)
+    return "fmt.Println" .. "(" .. message .. ")"
+  end,
+  lua = function(message)
+    return "print" .. "(vim.inspect(" .. message .. "))"
+  end,
+}
 
 M.log_symbol = function()
   local current_word = vim.fn.expand("<cword>")
   local current_filename = utils.get_current_filename(true)
-
   local message = table.concat({
     '"',
     AUTO_LOG_PREFIX,
@@ -36,18 +42,9 @@ M.log_symbol = function()
     ']]", ',
     current_word,
   })
-
-  if
-    vim.bo.filetype == "typescript"
-    or vim.bo.filetype == "typescriptreact"
-    or vim.bo.filetype == "javascript"
-    or vim.bo.filetype == "javascriptreact"
-  then
-    vim.cmd("norm oconsole.log(" .. message .. ");")
-  elseif vim.bo.filetype == "go" then
-    vim.cmd("norm ofmt.Println(" .. message .. ")")
-  elseif vim.bo.filetype == "lua" then
-    vim.cmd("norm oprint(" .. message .. ")")
+  local log_func = filetype_log_map[vim.bo.filetype]
+  if log_func then
+    vim.cmd("norm o" .. log_func(message))
   end
 end
 
@@ -56,29 +53,20 @@ M.delete_logs = function()
   local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
   local new_lines = {}
 
-  -- Get the print command for the current filetype
-  local print_cmd = get_auto_print_cmd(vim.bo.filetype)
+  local log_func = filetype_log_map[vim.bo.filetype]
+  local print_cmd = log_func and log_func('"' .. AUTO_LOG_PREFIX .. '"') or nil
 
-  -- Initialize a counter for the deleted lines
   local deleted_lines = 0
   for _, line in ipairs(lines) do
-    -- Check if the line contains the print command
-    if string.find(line, print_cmd) == nil then
-      -- If not, add the line to the new lines
+    if print_cmd and string.find(line, print_cmd) == nil then
       table.insert(new_lines, line)
     else
-      -- Increment the counter for deleted lines
       deleted_lines = deleted_lines + 1
     end
   end
 
-  -- Replace the buffer's lines with the new lines
   vim.api.nvim_buf_set_lines(buffer, 0, -1, false, new_lines)
-
-  -- Use vim.notify to display the number of deleted lines
-  vim.notify("Deleted " .. deleted_lines .. " lines", vim.log.levels.INFO, {
-    title = "Auto Log",
-  })
+  vim.notify("Deleted " .. deleted_lines .. " lines", vim.log.levels.INFO, { title = "Auto Log" })
 end
 
 return M
