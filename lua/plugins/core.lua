b8ae57903e9c3dacf73d3a4e916f660f12ab2bec
nbr <nikolaus.brunner@protonmail.ch>
2023-09-21 12:06:12 +0200

    core(colorscheme): carbonfox

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -5,11 +5,12 @@ M.spec = {
   "LazyVim/LazyVim",
   opts = {
     -- colorscheme = "kanagawa",
-    colorscheme = "terra",
+    -- colorscheme = "terra",
     -- colorscheme = "tokyonight",
-    -- colorscheme = "carbonfox",
+    colorscheme = "carbonfox",
     -- colorscheme = "terafox",
     -- colorscheme = "nordfox",
+    -- colorscheme = "github_dark_high_contrast",
   },
 }
 
