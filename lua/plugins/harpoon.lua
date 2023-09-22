10206cd3b3f06501334545f1d98816b3a1ae2ca0
nbr <nikolaus.brunner@protonmail.ch>
2023-09-22 10:09:43 +0200

    plugs(harpoon): fix settings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -61,25 +61,27 @@ M.specs = {
     ---@diagnostic disable-next-line: assign-type-mismatch
     keys = M.keys(),
     opts = {
-      menu = { width = 75, height = 10 },
+      global_settings = {
+        menu = { width = 75, height = 10 },
 
-      -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-      save_on_toggle = true,
+        -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+        save_on_toggle = true,
 
-      -- saves the harpoon file upon every change. disabling is unrecommended.
-      save_on_change = true,
+        -- saves the harpoon file upon every change. disabling is unrecommended.
+        save_on_change = true,
 
-      -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-      enter_on_sendcmd = false,
+        -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+        enter_on_sendcmd = false,
 
-      -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-      tmux_autoclose_windows = false,
+        -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+        tmux_autoclose_windows = false,
 
-      -- filetypes that you want to prevent from adding to the harpoon list menu.
-      excluded_filetypes = { "harpoon" },
+        -- filetypes that you want to prevent from adding to the harpoon list menu.
+        excluded_filetypes = { "harpoon" },
 
-      -- set marks specific to each git branch inside git repository
-      mark_branch = true,
+        -- set marks specific to each git branch inside git repository
+        mark_branch = true,
+      },
     },
   },
 
