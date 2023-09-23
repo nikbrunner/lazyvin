336d9b959020875b9ee4e118bdcdde84c3cdbee1
nbr <nikolaus.brunner@protonmail.ch>
2023-09-23 13:05:26 +0200

    opts: cleanup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,5 @@
--- Global WhichKeyIgnore label
 WhichKeyIgnoreLabel = "which_key_ignore"
 
 vim.opt.swapfile = false -- No swapfiles
-vim.o.showtabline = 2 -- Always show tabline
-vim.o.cursorline = false -- Highlight current line
+vim.opt.showtabline = 2 -- Always show tabline
+vim.opt.cursorline = false -- Highlight current line
