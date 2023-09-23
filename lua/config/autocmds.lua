8bb02335b013b31e07dc10fd1916a436e83158c1
nbr <nikolaus.brunner@protonmail.ch>
2023-09-23 10:58:04 +0200

    autocmds: close certain filetypes with <ESC> in normal mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,24 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+local create_autocmd = vim.api.nvim_create_autocmd
+
+-- Close these filetypes with <Esc> in normal mode
+create_autocmd("FileType", {
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "notify",
+    "lspinfo",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "PlenaryTestPopup",
+    "diagmsg",
+    "chatpgpt",
+    "ObsidianBacklinks",
+    "fzf",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "<Esc>", "<cmd>close<CR>", { buffer = event.buf, silent = true })
+  end,
+})
