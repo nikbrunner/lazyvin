c2052435aa6525158a410851d2b6b14cd6ce7333
nbr <nikolaus.brunner@protonmail.ch>
2023-10-04 13:33:29 +0200

    plugs(cmp): update maps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -15,6 +15,20 @@ M.spec = {
         border = "rounded",
       }),
     }
+
+    opts.mapping = cmp.mapping.preset.insert({
+      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-u>"] = cmp.mapping.scroll_docs(4),
+      ["<C-f>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<S-CR>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    })
   end,
 }
 
