056b6a0eb0c51160fba82bcc5c3cb1e1faa2ff4b
nbr <nikolaus.brunner@protonmail.ch>
2023-10-07 17:20:00 +0200

    core: clean up

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,17 +1,14 @@
 local M = {}
 
+---@alias LazyVin.Colorscheme "kanagawa" | "tokyonight-moon" | "carbonfox" | "terafox" | "nordfox" | "terra_fall_night" | "terra_spring_night"
+
 ---@type LazySpec
 M.spec = {
   "LazyVim/LazyVim",
   opts = {
-    -- colorscheme = "kanagawa",
-    -- colorscheme = "terra_fall_night",
-    colorscheme = "terra_spring_night",
-    -- colorscheme = "terra_control_night",
-    -- colorscheme = "tokyonight-moon",
-    -- colorscheme = "carbonfox",
-    -- colorscheme = "terafox",
-    -- colorscheme = "nordfox",
+
+    ---@type LazyVin.Colorscheme
+    colorscheme = "tokyonight-moon",
   },
 }
 
