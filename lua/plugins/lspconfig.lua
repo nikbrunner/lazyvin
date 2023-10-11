0f5345e2caa80ee845ef4a681a034299b400a706
nbr <nikolaus.brunner@protonmail.ch>
2023-10-11 09:10:51 +0200

    plugs(glance): resolve
    use trouble

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,12 +7,7 @@ M.spec = {
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
-    -- If `glance.nvim` is available then use it for LSP References
-    if require("lazyvim.util").has("glance.nvim") then
-      keys[#keys + 1] = { "gr", "<cmd>Glance references<CR>", desc = "LSP References (Glance)" }
-    end
-
-    keys[#keys + 1] = { "gR", "<cmd>Trouble lsp_references<CR>", desc = "LSP References (Trouble)" }
+    keys[#keys + 1] = { "gr", "<cmd>Trouble lsp_references<CR>", desc = "LSP References (Trouble)" }
   end,
   ---@class PluginLspOpts
   opts = {
