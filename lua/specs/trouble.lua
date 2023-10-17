dac18157b8fa6f38892fc146be211eef446e914f
nbr <nikolaus.brunner@protonmail.ch>
2023-10-17 19:28:50 +0200

    specs(trouble): update

--- lua/specs/trouble.lua
+++ lua/specs/trouble.lua
@@ -9,9 +9,9 @@ M.spec = {
     mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
     group = true, -- group results by file
     padding = true, -- add an extra new line on top of the list
-    win_config = { border = "rounded" }, -- window configuration for floating windows. See |nvim_open_win()|.
+    win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
     auto_close = true, -- automatically close the list when you have no diagnostics
-    auto_fold = true, -- automatically fold a file trouble list at creation
+    auto_fold = false, -- automatically fold a file trouble list at creation
   },
 }
 
