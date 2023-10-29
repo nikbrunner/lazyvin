2e6f7e7432dbe4c826f3c3878ecbad3e8af0abf5
nbr <nikolaus.brunner@protonmail.ch>
2023-10-29 19:26:25 +0100

    specs(telescope): fix history bindings

--- lua/specs/telescope.lua
+++ lua/specs/telescope.lua
@@ -182,8 +182,8 @@ M.spec = {
             i = {
               ["<ESC>"] = actions.close,
 
-              ["<C-h>"] = actions.cycle_history_next,
-              ["<C-l>"] = actions.cycle_history_prev,
+              ["<C-l>"] = actions.cycle_history_next,
+              ["<C-h>"] = actions.cycle_history_prev,
 
               ["<C-k>"] = actions.move_selection_previous,
               ["<C-j>"] = actions.move_selection_next,
