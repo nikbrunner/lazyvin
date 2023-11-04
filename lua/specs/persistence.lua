ba5ed5da4f79be031d85a6c6ba3b4cae9b518c16
nbr <nikolaus.brunner@protonmail.ch>
2023-11-04 17:42:18 +0100

    specs(persistence): disable

--- lua/specs/persistence.lua
+++ lua/specs/persistence.lua
@@ -5,37 +5,6 @@ local M = {}
 M.spec = {
   "folke/persistence.nvim",
   enabled = false,
-  event = "BufReadPre",
-  opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
-  keys = {
-    -- Disable default LazyVim mappings
-    { "<leader>qs", false },
-    { "<leader>ql", false },
-    { "<leader>qd", false },
-
-    -- New mappings
-    {
-      "<leader>Sr",
-      function()
-        require("persistence").load()
-      end,
-      desc = "Restore Session",
-    },
-    {
-      "<leader>Sl",
-      function()
-        require("persistence").load({ last = true })
-      end,
-      desc = "Restore Last Session",
-    },
-    {
-      "<leader>Sd",
-      function()
-        require("persistence").stop()
-      end,
-      desc = "Don't Save Current Session",
-    },
-  },
 }
 
 return M.spec
