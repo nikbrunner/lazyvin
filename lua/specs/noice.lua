c45525f2f54900b1c2a1f7b42df608aaf99dba70
nbr <nikolaus.brunner@protonmail.ch>
2023-11-14 22:54:39 +0100

    refactor(noice): clean out spec

--- lua/specs/noice.lua
+++ lua/specs/noice.lua
@@ -16,40 +16,15 @@ M.spec = {
         },
       },
     },
-    routes = {
-      -- don't show messages when buffer is written
-      {
-        filter = {
-          event = "msg_show",
-          kind = "",
-          find = "written",
-        },
-        opts = { skip = true },
-      },
-      {
-        view = "notify",
-        filter = { event = "msg_showmode" },
-      },
-    },
     messages = {
       enabled = true, -- enables the Noice messages UI
     },
-    -- NOTE: Notify causes flickering on the Cursor in some Terminals
-    -- I noticed it in iTerm and also Warp
-    -- Kitty seems to be the only one without this issue
-    notify = {
-      enabled = false,
-      view = "notify",
-    },
     lsp = {
-      progress = {
-        enabled = false,
-      },
       hover = {
         ---@type NoiceViewOptions
         opts = {
           border = {
-            style = "single",
+            style = "rounded",
             padding = { 0, 2 },
           },
         },
