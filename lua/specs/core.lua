fd0b65a03a47d9c88e8da7d2639250bc2ed9465d
nbr <nikolaus.brunner@protonmail.ch>
2023-11-15 18:15:22 +0100

    style(colorscheme): terra_fall_night

--- lua/specs/core.lua
+++ lua/specs/core.lua
@@ -5,7 +5,7 @@ M.spec = {
   "LazyVim/LazyVim",
   opts = {
 
-    colorscheme = "terra_winter_night",
+    colorscheme = "terra_fall_night",
 
     ---@type table<string, string[]>?
     kind_filter = {
