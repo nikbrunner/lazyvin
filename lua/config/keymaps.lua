dc66e24a09c5100697dfbedd56edfc7d589c09ad
nbr <nikolaus.brunner@protonmail.ch>
2023-11-17 13:28:08 +0100

    feat(keymaps): add default opts for set

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,12 @@
 local lib = require("lib")
 local lazyvim_util = require("lazyvim.util")
 
--- TODO: add options
-local set = vim.keymap.set
+local function set(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.noremap = opts.noremap == nil and true or opts.noremap
+  opts.silent = opts.silent == nil and true or opts.silent
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
 local del = vim.keymap.del
 
 -- lib.edit.discipline()
