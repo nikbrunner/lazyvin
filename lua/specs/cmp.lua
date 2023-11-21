f26f8e5c46e40457fe6cc2f81e18151be5b61de8
nbr <nikolaus.brunner@protonmail.ch>
2023-11-21 11:51:49 +0100

    feat(cmp): trigger suggest on <c-l>
    Must be changed because I use <c-space> for tmux prefix now

--- lua/specs/cmp.lua
+++ lua/specs/cmp.lua
@@ -20,7 +20,7 @@ M.spec = {
       ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-u>"] = cmp.mapping.scroll_docs(4),
-      ["<C-space>"] = cmp.mapping.complete(),
+      ["<C-l>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       ["<S-CR>"] = cmp.mapping.confirm({
