9cb63018e223f289f066d2de1f980c89647b904e
nbr <nikolaus.brunner@protonmail.ch>
2023-12-30 07:32:43 +0100

    feat(specs): align mappings for fzf with vin

--- lua/specs/fzf.lua
+++ lua/specs/fzf.lua
@@ -114,17 +114,17 @@ M.specs = {
     keys = {
       {
         "<leader><space>",
-        M.fzf("oldfiles", {
+        M.fzf("files", {
           winopts = M.win_presets.small.no_preview,
         }),
         desc = "Files",
       },
       {
-        "<leader>r",
-        M.fzf("buffers", {
+        "<leader>,",
+        M.fzf("oldfiles", {
           winopts = M.win_presets.small.no_preview,
         }),
-        desc = "Buffers",
+        desc = "History",
       },
       -- <leader>ca is mapped via lua/specs/lspconfig.lua
       {
