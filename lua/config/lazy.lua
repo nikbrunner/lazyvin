15ece0b583a801f65826030970e3f193e1ce831c
nbr <nikolaus.brunner@protonmail.ch>
2023-12-30 09:13:40 +0100

    feat(lazy): disable netrw

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,9 +50,7 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
