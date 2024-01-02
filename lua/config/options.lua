3d2f3a2a4e851955443ca1feef25969b8798e439
nbr <nikolaus.brunner@protonmail.ch>
2024-01-02 17:58:20 +0100

    feat(options): update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,5 @@ WhichKeyIgnoreLabel = "which_key_ignore"
 
 vim.opt.swapfile = false
 vim.opt.showtabline = 0
-vim.opt.cursorline = true
-vim.opt.timeoutlen = 250
+vim.opt.cursorline = false
 vim.opt.spelllang = "en_us,de_de"
